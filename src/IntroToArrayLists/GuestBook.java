a5aa309a44bc7ad3629e09208ea2c31b777b7cb8
JackChine27 <league@WTS14.attlocal.net>
2017-09-03 17:29:46 -0700

    gljhljsfhlihlsf

--- src/IntroToArrayLists/GuestBook.java
+++ src/IntroToArrayLists/GuestBook.java
@@ -1,5 +1,11 @@
 package IntroToArrayLists;
 
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class GuestBook {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
@@ -9,4 +15,29 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton addName = new JButton();
+	JButton viewNames = new JButton();
+	
+	public static void main(String[] args) {
+		
+		GuestBook g = new GuestBook();
+		
+		ArrayList<String> guestList = new ArrayList<String>();
+		
+		g.setup();
+		
+	}
+	
+	void setup() {
+		frame.add(panel);
+		panel.add(addName);
+		panel.add(viewNames);
+		addName.setText("Add Name");
+		viewNames.setText("View Names");
+		frame.setVisible(true);
+		frame.pack();
+	}
 }
--- src/IntroToArrayLists/IntroToArrayLists.java
+++ src/IntroToArrayLists/IntroToArrayLists.java
@@ -1,20 +1,56 @@
 package IntroToArrayLists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
+		ArrayList<String> strings = new ArrayList<String>();
 		
 		//2. Add five Strings to your list
+		strings.add("this is the first string");
+		strings.add("this is the second string");
+		strings.add("this is the fourth string");
+		strings.add("no, it's not broken");
+		strings.add("Exception in thread \"main\" java.lang.Error: Unresolved compilation problem: \n" + 
+				"	Syntax error, insert \";\" to complete Statement\n" + 
+				"\n" + 
+				"	at IntroToArrayLists.IntroToArrayLists.main(IntroToArrayLists.java:16)");
 		
 		//3. Print all the Strings using a standard for-loop
+		for (int i = 0; i < strings.size(); i++) {
+			String j = strings.get(i);
+			System.out.println(j);
+		}
 		
 		//4. Print all the Strings using a for-each loop
+		System.out.println("\n");
+		for (String s : strings) {
+			System.out.println(s);
+		}
 		
 		//5. Print only the even numbered elements in the list.
+		System.out.println("\n");
+		for (int i = 0; i < strings.size(); i += 2) {
+			String j = strings.get(i);
+			System.out.println(j);
+		}
 		
 		//6. Print all the Strings in reverse order.
+		System.out.println("\n");
+		for (int i = strings.size() - 1; i >= 0; i--) {
+			String j = strings.get(i);
+			System.out.println(j);
+		}
 		
 		//7. Print only the Strings that have the letter 'e' in them.
+		System.out.println("\n");
+		for (int i = 0; i < strings.size(); i++) {
+			String j = strings.get(i);
+			if (j.contains("e")) {
+				System.out.println(j);
+			}
+		}
 	}
 }
